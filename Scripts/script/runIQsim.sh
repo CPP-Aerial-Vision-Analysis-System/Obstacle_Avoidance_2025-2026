65722dc71c8b741e070a85c157fa050e5c0c5cda
pixelatedknight27 <maxgross72@gmail.com>
2025-05-27 00:59:05 -0700

    make iq_sim launch file

--- Scripts/script/runIQsim.sh
+++ Scripts/script/runIQsim.sh
@@ -1,11 +1,11 @@
-#!/usr/bin
+#!/bin/bash
 
-gnome-terminal --title Gazebo --working-directory=/home/suas/Desktop/Obstacle_Avoidance_2024-2025/ROS-IQ-Tutorials -e "bash -c 'roslaunch iq_sim lidar.launch'"
+gnome-terminal --title Gazebo --working-directory=$HOME/catkin_ws -- bash -c "roslaunch iq_sim lidar.launch"
 
 sh ~/catkin_ws/src/Obstacle_Avoidance_2025-2026/Scripts/script/runSitl.sh
 
 sleep 10
 
-gnome-terminal --title MavROS --working-directory=/home/suas/Desktop/Obstacle_Avoidance_2024-2025/ROS-IQ-Tutorials -e "bash -c 'roslaunch iq_gnc apm.launch'"
+gnome-terminal --title MavROS --working-directory=$HOME/catkin_ws -- bash -c "roslaunch iq_gnc apm.launch"
 
-gnome-terminal --title IQAvoidance --working-directory=/home/suas/Desktop/Obstacle_Avoidance_2024-2025/ROS-IQ-Tutorials -e "bash -c 'rosrun iq_gnc avoidance3D_sol'"
+gnome-terminal --title IQAvoidance --working-directory=$HOME/catkin_ws -- bash -c "rosrun iq_gnc avoidance_sol"
--- Scripts/script/runSitl.sh
+++ Scripts/script/runSitl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --title MavProxy --working-directory=/home/suas/Desktop/Obstacle_Avoidance_2024-2025/ROS-IQ-Tutorials -e "bash -c 'cd ~/ardupilot/ArduCopter/ && ../Tools/autotest/sim_vehicle.py -v ArduCopter -f gazebo-iris --console --out=udp:127.0.0.1:14551'"
+gnome-terminal  --title MavProxy --working-directory=$HOME/ardupilot/ArduCopter/ -- bash -c "../Tools/autotest/sim_vehicle.py -v ArduCopter -f gazebo-iris --console --out=udp:127.0.0.1:14551"
+
+exit 0
